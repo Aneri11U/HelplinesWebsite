ef4597ddf8acd60cca088ecacf944f31d4e5a5a9
Aneri11U <121008198+Aneri11U@users.noreply.github.com>
2023-07-27 21:11:52 +0530

    Add files via upload

---
+++ ap_js.js
@@ -0,0 +1,23 @@
+var app = angular.module('AP',[]);
+		app.controller('Ctrl-AP',function($scope){
+            $scope.andhra[
+                {helpline1:'CENTRALIZED HELPLINE NO',number1:'112'},
+                {helpline1:'COVID 19 HELPLINE',number1:'011-23978046'},
+                {helpline1:'A.P. COVID 19 HELPLINE',number1:'0866-2410978'},
+                {helpline1:'POLICE',number1:'100'},
+                {helpline1:'FIRE',number1:'101'},
+                {helpline1:'AMBULANCE',number1:'102'},
+                {helpline1:'DISHA HELPLINE',number1:'181'},
+                {helpline1:'SPANDANA ( CITIZEN GREVIENCE CENTRE )',number1:'1902'},
+                {helpline1:'CORRUPTION GREVIENCES',number1:'14400'},
+                {helpline1:'Railway Enquiry',number1:'139'},
+                {helpline1:'A.M.O.H-I HOSPITAL, Vijayawada',number1:'9866514148'},
+                {helpline1:'Government General Hospital, Gunadala, Vijayawada',number1:'0866-2576101'},
+                {helpline1:'Government General Hospital,Hanumanpet Vijayawada',number1:'0866-2576027'},
+                {helpline1:'ANDHRA HOSPITAL , VIJAYWADA',number1:'0866-2417577'},
+                {helpline1:'ANDHRA HOSPITAL HEART AND BRAIN ',number1:'+91 866 2574757'},
+                {helpline1:'Medical Helpline in Andhra Pradesh, Gujarat, Uttarakhand, Goa, Tamil Nadu, Rajasthan, Karnataka, Assam, Meghalaya, M.P And U.P',number1:'108'},
+                {helpline1:'Tourist Helpline',number1:' 040-23262152'},
+                {helpline1:'COOKING GAS LEAKAGE',number1:'040-27801123'}
+            ]
+        });
---
+++ js_file_project.js
@@ -0,0 +1,76 @@
+
+var app = angular.module('Imp',[]);
+		app.controller('Ctrl',function($scope){
+            $scope.data=[
+                {helpline:'COVID 19 HELPLINE',number:'011-23978046'},
+                {helpline:'NATIONAL EMERGENCY NUMBER',number:'112'},
+                {helpline:'POLICE',number:'100'},
+                {helpline:'FIRE',number:'101'},
+                {helpline:'AMBULANCE',number:'102'},
+                {helpline:'Disaster Management Services',number:'108'},
+                {helpline:'Women Helpline',number:'1091'},
+                {helpline:'Women Helpline - ( Domestic Abuse )',number:'181'},
+                {helpline:'Air Ambulance',number:'9540161344'},
+                {helpline:'Aids Helpline',number:'1097'},
+                {helpline:'Anti Poison ( New Delhi )',number:'011-1066'},
+                {helpline:'Disaster Management ( N.D.M.A )',number: '011-26701728'},
+                {helpline:'EARTHQUAKE / FLOOD / DISASTER ( N.D.R.F Headquaters )  NDRF HELPLINE',number:'011-24363260'},
+                {helpline:'Deputy Commissioner Of Police – Missing Child And Women',number:'1094'},
+                {helpline:'Railway Enquiry',number:'139'},
+                {helpline:'Senior Citizen Helpline',number:'1091'},
+                {helpline:'Medical Helpline in Andhra Pradesh, Gujarat, Uttarakhand, Goa, Tamil Nadu, Rajasthan, Karnataka, Assam, Meghalaya, M.P And U.P',number:'108'},
+                {helpline:'Railway Accident Emergency Service',number:'1072'},
+                {helpline:'Road Accident Emergency Service',number:'1073'},
+                {helpline:'Road Accident Emergency Service On National Highway For Private Operators',number:'1033'},
+                {helpline:'ORBO Centre, AIIMS (For Donation Of Organ) Delhi',number:'1060'},
+                {helpline:'Kisan Call Centre',number:'1551'},
+                {helpline:'Relief Commissioner For Natural Calamities',number:'1070'},
+                {helpline:'Children In Difficult Situation',number:'1098'},
+                {helpline:'Central Vigilance Commission',number:'1964'},
+                {helpline:'Tourist Helpline',number:'1363'},
+                {helpline:'LPG Leak Helpline',number:'1906'},
+                {helpline:'INDIAN RALWAYS  Enquiry',number:'139'},
+                {helpline:'I.R.D.A ( Insurance Regulatory and Development Authority )',number:'155255'},
+                {helpline:'HELPLINE OF NDMC CONTROL ROOM ( DELHI )',number:'1267'},
+                {helpline:'NATIONAL POISION INFORMATION CENTRE ( 24*7 )',number:'011-26593677'},
+                {helpline:'BIKE AMBULANCE FOR HEART ATTACK - ( Initative by AIIMS AND ICMR )',number:'1800-111-044'}
+            ];
+
+            $scope.states=[
+                {s:'Andhra Pradesh'},
+                {s:'Arunachal Pradesh'},
+                {s:'Assam'},
+                {s:'Bihar'},
+                {s:'Chandigrah'},
+                {s:'Chatishgrah'},
+                {s:'Delhi'},
+                {s:'Goa'},
+                {s:'Gujarat'},
+                {s:'Haryana'},
+                {s:'Himachal Pradesh'},
+                {s:'Jharkhand'},
+                {s:'Jammu Kashmir'},
+                {s:'Karnataka'},
+                {s:'kerala'},
+                {s:'Maharashtra'},
+                {s:'Manipur'},
+                {s:'Madhya Pradesh'},
+                {s:'Meghalaya'},
+                {s:'Mizoram'},
+                {s:'Nagaland'},
+                {s:'Odisha'},
+                {s:'Panjab'},
+                {s:'Rajasthan'},
+                {s:'Sikkim'},
+                {s:'Tamil Nadu'},
+                {s:'Telangana'},
+                {s:'Tripura'},
+                {s:'Uttar Pradesh'},
+                {s:'Uttarakhand'},
+                {s:'West Bengal'}
+            ];
+
+           
+
+});
+
